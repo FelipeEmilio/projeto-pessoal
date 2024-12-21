e6db5ff4b9783f9382c7718af364141e008edbf3
Felipe Emilio <felipeemilio27@gmail.com>
2024-12-21 13:14:00 -0300

    começando o header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header/index';
+
+function App() {
+  return (
+    <body className="App">
+      <Header />
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export const Header = () => {
+
+    return(
+        <header className='headers'>
+            <h2 className='teste'>Teste</h2>
+        </header>
+    )
+
+}
